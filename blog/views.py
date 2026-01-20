50886d3cff659762adea4f2a678dfaafd969fe30
Himanshu Yadav <asherkingpin@gmail.com>
2026-01-20 15:15:22 +0530

    change in view.py

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-
-posts = [
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 01, 2026'
-    },
-    {
-        'author':'EthanHunt ',
-        'title':'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 02, 2026'
-    }
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
