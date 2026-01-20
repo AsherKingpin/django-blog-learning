196ff490b3d8d7b81e458243a40cdd29ba9ab1cb
Himanshu Yadav <asherkingpin@gmail.com>
2026-01-20 14:38:28 +0530

    Initial Django project

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+
+posts = [
+    {
+        'author':'CoreyMS',
+        'title':'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 01, 2026'
+    },
+    {
+        'author':'EthanHunt ',
+        'title':'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 02, 2026'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
